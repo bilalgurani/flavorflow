c56cb5e102d3a87893cc9758c9763d17154bc2eb
Bilal Gurani <bilalgurani@Bilals-MacBook-Pro.local>
2025-06-17 14:33:00 +0530

    Add Recipe and custom directive

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { Recipe } from '../recipe.model';
+import { DropDownDirective } from '../../shared/dropdown.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-detail',
-  imports: [],
+  imports: [CommonModule, DropDownDirective],
   templateUrl: './recipe-detail.component.html',
   styleUrl: './recipe-detail.component.scss'
 })
