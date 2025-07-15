eda888dec8a4a4cbd20f59382be7acb256307072
Bilal Gurani <bilalgurani@Bilals-MacBook-Pro.local>
2025-07-15 23:53:04 +0530

    Add create new recipe Reactive form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Params } from '@angular/router';
+import { RecipeService } from '../recipes.service';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -10,14 +12,33 @@ import { ActivatedRoute, Params } from '@angular/router';
 export class RecipeEditComponent implements OnInit {
   id!: number;
   editMode = false;
+  recipeForm!: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
 
   ngOnInit(): void {
     this.route.params
     .subscribe((params: Params) => {
       this.id = +params['id'];
       this.editMode = params['id'] != null;
+      this.initForm();
+    })
+  }
+
+  private initForm() {
+    let recipeName = '';
+    let recipeUrl = '';
+    let recipeDescription = '';
+    if (this.editMode) {
+      const recipe = this.recipeService.getRecipe(this.id);
+      recipeName = recipe.name;
+      recipeUrl = recipe.imageUrl;
+      recipeDescription = recipe.description;
+    }
+    this.recipeForm = new FormGroup({
+      'name': new FormControl(recipeName),
+      'image-url': new FormControl(recipeUrl),
+      'description': new FormControl(recipeDescription)
     })
   }
 }
